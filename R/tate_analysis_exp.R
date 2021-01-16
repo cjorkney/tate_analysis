e7a7a934f3f2ec3b99e5037e2b21866d4917b4ae
Callum Orkney <callum.orkney@gmail.com>
2021-01-16 16:42:34 +0000

    Begin cleaning artists data

--- R/tate_analysis_exp.R
+++ R/tate_analysis_exp.R
@@ -1,6 +1,25 @@
 library(tidytuesdayR)
+library(dplyr)
+library(tidyr)
+library(stringr)
 
 tuesdata <- tidytuesdayR::tt_load('2021-01-12')
 
 artwork <- tuesdata$artwork
 artists <- tuesdata$artists
+
+
+# Clean datasets ----------------------------------------------------------
+
+artists_clean <- artists %>% 
+  mutate(
+    across(c("id", "gender"), ~ as.factor(.x)),
+    bplace_commas = str_count(placeOfBirth, ","),
+    placeOfBirth = str_replace(placeOfBirth, ",\\s.+,", ","),
+    ) %>% 
+  separate(placeOfBirth,
+           into = c("birth_city", "birth_country"), sep = ", ",
+           remove = FALSE)
+  
+  
+         
